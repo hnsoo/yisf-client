4e8d08d0d6dba9392e896ddcb8f9dc779bc5d495
hnsoo <khsoo2439@gmail.com>
2022-05-21 09:58:39 +0900

    fix: TopBar components 개발

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from "styled-components";
+import {HiOutlineLogout} from 'react-icons/hi'
+
+export default function TopBar() {
+    return (
+      <Container>
+          <div></div>
+          <Timer>
+            35:24:50
+          </Timer>
+          <LogoutContainer>
+            <LogoutImg>
+                <HiOutlineLogout size="20px" color="white"/>
+            </LogoutImg>
+            <LogoutText>Log out</LogoutText>
+          </LogoutContainer>
+      </Container>
+    );
+}
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 35px;
+  background: #3A2D34;
+  justify-content: space-between;
+`
+const Timer = styled.div`
+  color: white;
+  font-size: 1.1rem;
+  padding-top: 4px;
+`
+
+const LogoutContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 3px;
+`
+
+const LogoutImg = styled.div`
+  width: 20px;
+  height: 20px;
+  padding-top: 3px;
+  padding-right: 7px;
+`
+const LogoutText = styled.div`
+  color: white;
+  font-size: 1.1rem;
+  padding-right: 15px;
+`
