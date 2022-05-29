73246f0e6819cdb77acf637134ae0fbea9012789
hnsoo <khsoo2439@gmail.com>
2022-05-29 19:50:26 +0900

    리덕스 action 작성

---
+++ src/redux/action.js
@@ -0,0 +1,19 @@
+import AuthService from "../service/Auth";
+
+export const login = (id, password) => (dispatch) => {
+    return AuthService.login(id, password).then(
+        (data) => {
+            dispatch({
+                type: "LOGIN_SUCCESS",
+                payload: { token: data },
+            });
+            return Promise.resolve();
+        },
+        () => {
+            dispatch({
+                type: "LOGIN_FAIL",
+            });
+            return Promise.reject();
+        }
+    );
+};
