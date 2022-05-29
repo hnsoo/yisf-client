aed0bfd5ea53ea6120888899a88edc808662f640
hnsoo <khsoo2439@gmail.com>
2022-05-29 19:50:40 +0900

    리덕스 reducers 작성

---
+++ src/redux/reducers.js
@@ -0,0 +1,27 @@
+const token = localStorage.getItem("token");
+
+const initialState = token
+    ? { isLoggedIn: true, token}
+    : { isLoggedIn: false, token: null};
+
+export default function auth(state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        case "LOGIN_SUCCESS":
+            return {
+                ...state,
+                isLoggedIn: true,
+                token: payload.token,
+            };
+        case "LOGIN_FAIL":
+            return {
+                ...state,
+                isLoggedIn: false,
+                token: null,
+            };
+
+        default:
+            return state;
+    }
+}
+
