b48ad90308de199204aab558b5ce4164826eeea3
hnsoo <khsoo2439@gmail.com>
2022-06-15 16:50:33 +0900

    마이페이지 내용 추가

--- src/components/dock/Mypage.jsx
+++ src/components/dock/Mypage.jsx
@@ -3,11 +3,22 @@ import styled from "styled-components";
 
 export default function Mypage(){
     return (
-        <div>
-            <p>User name</p>
-            <p>E-mail</p>
-            <p>My score</p>
-            <p>My solved</p>
-        </div>
+        <Container>
+            <b>User name</b><br/>
+            khsoo2439<br/><br/>
+            <b>E-mail</b><br/>
+            khsoo2439@naver.com<br/><br/>
+            <b>My score</b><br/>
+            1000<br/><br/>
+            <b>My solved</b><br/>
+
+        </Container>
     );
 }
+const Container = styled.div`
+  //display: flex;
+  text-align: left;
+  flex-direction: column;
+  padding: 0.25rem;
+`
+
