6c61a296266733f4ba5dfe41378667e8b23fd1cf
hnsoo <khsoo2439@gmail.com>
2022-06-15 16:46:38 +0900

    공지사항 내용 추가

--- src/components/dock/Notice.jsx
+++ src/components/dock/Notice.jsx
@@ -3,10 +3,19 @@ import styled from "styled-components";
 
 export default function Notice({color, img}){
     return (
-        <div>
-            <p>공지사항</p>
-            <p>대회목적</p>
-            <p>대회규칙</p>
-        </div>
+        <Container>
+            <b>공지사항</b><br/>
+            공지사항입니다.<br/><br/>
+            <b>대회목적</b><br/>
+            대회목적입니다.<br/><br/>
+            <b>대회규칙</b><br/>
+            대회규칙입니다.<br/><br/>
+        </Container>
     );
 }
+const Container = styled.div`
+  //display: flex;
+  text-align: left;
+  flex-direction: column;
+  padding: 0.25rem;
+`
