902a3f7357eec4025a6e429ed82f98609f1924c0
hnsoo <khsoo2439@gmail.com>
2022-06-18 00:07:59 +0900

    fetch Promise 기능 분리: user

--- src/service/user.js
+++ src/service/user.js
@@ -1,4 +1,5 @@
 import AuthService from "./auth";
+import RequestService from "./request"
 
 const API_URL = "http://15.165.86.75:8080/api/v1"
 
@@ -12,27 +13,9 @@ class UserService {
                         Authorization: "Bearer " + localStorage.getItem("token"),
                     }
                 })
-                    .then((res) => {
-                        // reponse가 ok가 아닐 때
-                        console.log(res)
-                        if (!res.ok) {
-                            console.log(res.json())
-                            throw new Error('400 or 500 에러 발생')
-                        }
-                        return res.json()
-                    })
-                    .then((result) => {
-                            // if(result.errorCode){
-                            //     throw new Error(result.detail)
-                            // }
-                            console.log(result)
-                            return result;
-                        }
-                    )
-                    .catch((err) => {
-                        console.log(err)
-                        return Promise.reject();
-                    })
+                    .then((res) => RequestService.checkError(res))
+                    .then((result) => RequestService.retResult(result))
+                    .catch((err) => RequestService.handleError(err))
             )
             .catch(() =>
                 Promise.reject()
