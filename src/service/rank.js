99b704ee4dbc2e3f479d78c1d46aeed5306cf9ff
hnsoo <khsoo2439@gmail.com>
2022-06-18 23:28:22 +0900

    rank 마지막 풀이 시간 보기좋게 파싱

--- src/service/rank.js
+++ src/service/rank.js
@@ -17,12 +17,23 @@ class RankService {
                             "nickname": data.nickname,
                             "score": data.score,
                             "solvedCount": data.probIdList.length,
-                            "lastSolvedTime": data.lastAuthTime,
+                            "lastSolvedTime": this.parseTime(data.lastAuthTime),
                         })
                     )
                 }
             )
             .catch((err) => RequestService.handleError(err))
+    }
+    parseTime(time){
+        if(time){
+            let month = time.slice(5, 7);
+            let day = time.slice(8, 10);
+            let hour = time.slice(11, 13)
+            let minute = time.slice(14, 16)
+            let second = time.slice(17, 19)
+            return `${month}월 ${day}일 ${hour}시 ${minute}분 ${second}초`
+        }
+        return "no solved"
     }
 }
 
