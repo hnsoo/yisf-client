2dcbf728f02cfb180dc07bce16fa3ff2c70f7f74
hnsoo <khsoo2439@gmail.com>
2022-07-09 20:26:26 +0900

    공지사항 요소 UI 스타일 변경
    마우스를 올릴경우 마우스 모양 변경 및 공지사항 요소의 색상 변경

--- src/components/dock/NoticeElement.jsx
+++ src/components/dock/NoticeElement.jsx
@@ -28,10 +28,10 @@ export default function NoticeElement({elementIndex, info}) {
 }
 const Row = styled.tr`
   border-bottom: 1px solid lightgray;
-  // background: ${(props) => props.background ? "#cce8ff": "white"};
-  // :hover {
-  //   background: ${(props) => props.background ? "#cce8ff": "#e5f3ff"};
-  // };
+  :hover {
+    cursor: pointer;
+    background: ghostwhite;
+  };
 `
 const Col = styled.td`
   padding: 10px;
