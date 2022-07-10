29373eabcaf045b767bdba5dd4c47d03796f78ca
hnsoo <khsoo2439@gmail.com>
2022-07-10 20:05:16 +0900

    font 수정
    body와 b 태그에 폰트 적용

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Provider} from "react-redux";
 import store from "./redux/store";
+import "./App.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
