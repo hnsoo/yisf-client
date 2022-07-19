96f73ad2c2699314cbdc19fe47bd9c915d3eef90
hnsoo <khsoo2439@gmail.com>
2022-07-19 13:00:22 +0900

    list 속성 분리 및 변경
    리스트의 스타일을 component styled로 분리 + 리스트 마커의 색상 및 마진 조절

--- src/components/dock/mypage/SolvedList.jsx
+++ src/components/dock/mypage/SolvedList.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function SolvedList({problems}){
     const solvedList = () => {
         if(problems.length > 0){
             let result = []
             for(let problem of problems){
-                result.push(<li>{problem.title}</li>);
+                result.push(<Li>{problem.title}</Li>);
             }
-            return result;
+            return <Ul>{result}</Ul>;
         }
-        else return (<div style={{color: "gray"}}>no solved</div>)
+        else return (<NoSolved>no solved</NoSolved>)
     }
 
     return (solvedList())
 }
+const Ul = styled.ul`
+  font-size: 0.9rem;
+  text-align: left;
+  list-style-position: outside;
+`
+const Li = styled.li`
+  ::marker{
+    color: #e9a1a1;
+  }
+  list-style-position: inside;
+  margin-left: -30px;
+`
+const NoSolved = styled.div`
+  color: gray;
+  font-size: 0.9rem;
+`
