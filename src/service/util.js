176c74d0382fb1b0848307daf8bbf7edf66cc1ec
hnsoo <khsoo2439@gmail.com>
2022-08-10 21:33:14 +0900

    시간 계산 로직 오류 수정

--- src/service/util.js
+++ src/service/util.js
@@ -21,16 +21,19 @@ class UtilService {
         let now = new Date().getTime()
         let time = new Date(targetTime).getTime()
         let remainDate = time - now;
+        let _second = 1000;
+        let _minute = _second * 60;
+        let _hour = _minute * 60;
+        let _day = _hour * 24;
         // 목표 시간을 지났을 경우
         if (remainDate < 0)
             return result
         // 남은시간 % 하루 / 1시간 + (남은 Day * 24)
-        result['hours'] = Math.floor((remainDate % (1000 * 60 * 60 * 24)) / (1000*60*60) +
-            (remainDate / (1000 * 60 * 60 * 24)) * 24)
+        result['hours'] = Math.floor((remainDate % _day) / _hour) + Math.floor(remainDate / _day) * 24;
         // 남은시간 % 1시간 / 1분
-        result['minutes'] = Math.floor((remainDate % (1000 * 60 * 60)) / (1000*60))
+        result['minutes'] = Math.floor((remainDate % _hour) / _minute);
         // 남은시간 % 1분 / 1초
-        result['seconds'] = Math.floor((remainDate % (1000 * 60)) / 1000)
+        result['seconds'] = Math.floor((remainDate % _minute) / _second);
         return result
     }
     countDown(hours, minutes, seconds) {
